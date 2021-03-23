3682cbf20eda0c693d77b9d47d717ee86388b936
aigo880 <aigerim.ilip@mail.ru>
2021-03-23 14:42:52 +0600

    4th Day
    add playground file

--- 4Day.playground/Contents.swift
+++ 4Day.playground/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
 
-var str = "Hello, playground"
+// For loops
+let count = 1...10
+for number in count {
+    print("number - \(number)")
+}
+
+
+let albums = ["Red", "1989", "Reputation"]
+for album in albums {
+    print(album)
+}
+
+for _ in 1...5 {
+    print("GO")
+}
+
+
+// While loops
+var number = 1
+
+while number <= 20 {
+    print(number)
+    number += 1
+}
+
+
+// Repeat loops
+var number1 = 1
+
+repeat {
+    print(number1)
+    number1 += 1
+} while number1 < 5
+
+
+
+
+
