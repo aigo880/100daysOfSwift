3c60d1cbc664a11abe72921978c380ee420f1350
Aigo <aytumar.ilip@gmail.com>
2021-10-28 12:51:21 +0600

    Tasks

--- Day 1/day_1.playground/Contents.swift
+++ Day 1/day_1.playground/Contents.swift
@@ -4,3 +4,47 @@ import UIKit
 var greeting = "Hi!"
 greeting = "Howdy Ho!"
 
+// Strings and integers
+var userName = "John Doe" // string
+var userAge = 29 // integer
+userName = "Johny"
+userAge = 30
+
+var population = 8_000_000 // 8 000 000
+
+// Multi-line strings
+var longString1 = """
+long
+long
+string
+line
+"""
+var longString2 = """
+long
+long
+string
+line
+"""
+
+// Doubles and booleans
+var pi = 3.141592654 // Double
+var temperature = 38.5 // Double
+
+var isStudent = true
+
+// String interpolation
+var score = 85
+var newString = "Your score was \(score)" // Your score was 85
+var sumString = "Sum: 50 + 50 = \(50+50)" // Sum: 50 + 50 = 100
+
+// Constants
+let studentName = "Josh"
+
+// Type annotations
+let langName: String = "Apple Swift"
+let year: Int = 2014
+let release: Double = 5.5
+let isSwift: Bool = true
+
+
+
